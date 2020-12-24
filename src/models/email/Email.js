1141e915b8a095b9d8b244933ba7658f11f7960d
Jonas Brito <jonasexplore@gmail.com>
2020-12-23 21:44:47 -0300

    Separando ambiente de produção e testes.

--- src/models/email/Email.js
+++ src/models/email/Email.js
@@ -1,15 +1,36 @@
 const nodemailer = require("nodemailer");
 
+const emailConfigProduction = {
+  host: process.env.EMAIL_HOST,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  secure: true,
+};
+
+const emailConfigTest = (account) => ({
+  host: "smtp.ethereal.email",
+  auth: account,
+});
+
+async function createEmailConfiguration() {
+  if (process.env.NODE_ENV === "production") {
+    return emailConfigProduction;
+  } else {
+    const account = await nodemailer.createTestAccount();
+    return emailConfigTest(account);
+  }
+}
+
 class Email {
   async sendMail() {
-    const account = await nodemailer.createTestAccount();
-    const transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
-      auth: account,
-    });
-
+    const configEmail = await createEmailConfiguration();
+    const transporter = nodemailer.createTransport(configEmail);
     const info = await transporter.sendMail(this);
-    console.log("URL: ", nodemailer.getTestMessageUrl(info));
+    if (!process.env.NODE_ENV !== "production") {
+      console.log("URL: ", nodemailer.getTestMessageUrl(info));
+    }
   }
 }
 
